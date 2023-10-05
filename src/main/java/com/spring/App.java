831b3d97f9a2fe65889d5f73083bb8bbe02fd40e
thatpaiguy <54644670+thatpaiguy@users.noreply.github.com>
2023-10-05 14:23:38 -0400

    initialized

---
+++ src/main/java/com/spring/App.java
@@ -0,0 +1,36 @@
+package com.spring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+  public static void main(String[] args) {
+    // Set ChatGPT endpoint and API key
+    String endpoint = "https://api.openai.com/v1/chat/completions";
+    String apiKey = "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";
+        
+    // Prompt user for input string
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      System.out.print("Enter your message: ");
+      String input = reader.readLine();
+            
+      // Send input to ChatGPT API and display response
+      String response = ChatBot.sendQuery(input, endpoint, apiKey);
+      LOGGER.info("Response: {}", response);
+    } catch (IOException e) {
+      LOGGER.error("Error reading input: {}", e.getMessage());
+    } catch (JSONException e) {
+      LOGGER.error("Error parsing API response: {}", e.getMessage());
+    } catch (Exception e) {
+      LOGGER.error("Unexpected error: {}", e.getMessage());
+    }
+  }
+}
---
+++ src/test/java/com/spring/summarizer/SummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
